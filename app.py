7f92df3e95910f352dd2f2dc019ccbb1de0f56a1
aleksandrmalinoko <aleksandrmalinoko@gmail.com>
2026-02-14 13:56:25 +0300

    переходим на фичи

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from utils.sfera_api import generate_tasks, generate_tasks_test
+from utils.sfera_api import generate_tasks_dates, generate_tasks_label
 from utils.task_utils import group_tasks_by_assignee, group_tasks_by_system
 
 app = Flask(__name__)
 tasks = []
-
+label_to_match = None
 
 def sort_assignees(grouped_tasks):
     order = [
@@ -17,10 +17,11 @@ def sort_assignees(grouped_tasks):
         "Шереметьев Артемий Дмитриевич",
         "Новиков Ярослав Владимирович",
         "Твердохлеб Александр Григорьевич",
+        "Чернышев Андрей Викторович",
         "Коротких Евгений Олегович",
         "Бутенко Дмитрий Михайлович",
         "Котов Федор Васильевич",
-        "Сорокин Максим Владимирович",
+        "Мелихов Евгений Алексеевич",
         "Мельников Николай Сергеевич",
         "Гусева Мелания Андреевна",
         "Без исполнителя",
@@ -40,6 +41,7 @@ def sort_systems(grouped_systems):
         "1719 Проверка адреса",
         "1925 Машиночитаемые доверенности",
         "1512 Расширенный профиль клиента ЮЛ",
+        "3613 Расширенный профиль клиента ЮЛ (КНР)",
         "1717 Активности с корпоративным клиентом",
         "2032 Управление бизнес-командами Группы ВТБ",
         "Без системы",
@@ -47,6 +49,14 @@ def sort_systems(grouped_systems):
     ]
     ordered_keys = sorted(grouped_systems.keys(), key=lambda x: (order.index(x) if x in order else len(order)))
     return {key: grouped_systems[key] for key in ordered_keys}
+
+
+def generate_label(start, end):
+    # start: '2025-06-23', end: '2025-06-27' → '23.06-27.06.2025'
+    from datetime import datetime
+    sd = datetime.strptime(start, "%Y-%m-%d")
+    ed = datetime.strptime(end, "%Y-%m-%d")
+    return f"{sd.strftime('%d.%m')}-{ed.strftime('%d.%m')}.{ed.strftime('%Y')}"
 
 
 @app.route('/')
@@ -59,17 +69,22 @@ def query_page():
 
 @app.route('/fetch-tasks', methods=['POST'])
 def fetch_tasks():
-    """
-    Генерация задач на основе запроса и переход на страницу Kanban.
-    """
-    global tasks
-    query = request.form.get('query')
-    if not query:
-        return render_template('query.html', error="Пожалуйста, введите запрос!")
+    global tasks, label_to_match
+    mode = request.form.get('mode')
 
     try:
-        # Используем production-функцию для генерации задач
-        tasks = generate_tasks(query)
+        if mode == 'dates':
+            start_date = request.form.get('start_date')
+            end_date = request.form.get('end_date')
+            # Можно сформировать метку, либо сделать это внутри функции
+            label = generate_label(start_date, end_date)
+            tasks = generate_tasks_dates(start_date, end_date, label)
+            label_to_match = label
+        else:
+            label = request.form.get('query_label')
+            tasks = generate_tasks_label(label)
+            label_to_match = label
+
         return redirect(url_for('kanban'))
     except Exception as e:
         return render_template('query.html', error=f"Ошибка: {str(e)}")
@@ -77,9 +92,7 @@ def fetch_tasks():
 
 @app.route('/kanban')
 def kanban():
-    """
-    Страница с Kanban-доской.
-    """
+    global tasks, label_to_match
     grouped_tasks = group_tasks_by_assignee(tasks)
     grouped_systems = group_tasks_by_system(tasks)
 
@@ -87,7 +100,7 @@ def kanban():
     grouped_tasks = sort_assignees(grouped_tasks)
     grouped_systems = sort_systems(grouped_systems)
 
-    return render_template('kanban.html', grouped_tasks=grouped_tasks, grouped_systems=grouped_systems)
+    return render_template('kanban.html', grouped_tasks=grouped_tasks, grouped_systems=grouped_systems, label_to_match=label_to_match)
 
 
 if __name__ == '__main__':
--- utils/sfera_api.py
+++ utils/sfera_api.py
@@ -1,14 +1,157 @@
 import pickle
+import requests
+from datetime import datetime, timedelta
+import urllib3
+from utils import sfera_secrets
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def generate_tasks(query):
-    print(f"{query}")
-    with open('tasks_dict.pickle', 'rb') as f:
-        tasks = pickle.load(f)
-    return tasks
+BASE_URL = "https://sfera.inno.local"
+TASK_URL = f"{BASE_URL}/app/tasks/api/v1/entities"
+LOGIN_URL = f"{BASE_URL}/api/auth/login"
+sfera_attributes = ['number', 'name', 'description', 'relations', 'status', 'assignee', 'systems', 'dueDate', 'estimation', 'label', 'parent']
+sfera_area = 'SUPPGCCSR'
+atributes = ''
+for atr in sfera_attributes[:-1]:
+    atributes += f'{atr}%2C'
+atributes += sfera_attributes[-1]
 
 
-def generate_tasks_test():
-    with open('tasks_dict.pickle', 'rb') as f:
-        tasks = pickle.load(f)
-    return tasks
+def get_sfera_token():
+    get_token_headers = {
+        "Content-Type": "application/json"
+    } 
+    get_token_body = {
+        "username": sfera_secrets.sfera_user,
+        "password": sfera_secrets.sfera_password
+    }
+    response = requests.post(LOGIN_URL, json=get_token_body, headers=get_token_headers, verify=False)
+    token = response.json()['access_token']
+    return token
+
+
+def get_pages_count(token, sfera_query):
+    get_pages_headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    get_pages_url = f"https://sfera.inno.local/app/tasks/api/v1/entity-views?attributes=name&query={sfera_query}"
+    response = requests.get(get_pages_url, headers=get_pages_headers, verify=False, timeout=3600)
+    tasks_count = int(response.json()['totalPages'])
+    print(f"Фильтр вернул {tasks_count + 1} страниц")
+    return tasks_count
+
+
+def get_all_tasks(token, page, sfera_query):
+    page_size = f"page={page}&size=20&"
+    get_all_tasks_url = f"https://sfera.inno.local/app/tasks/api/v1/entity-views?{page_size}attributes={atributes}&query={sfera_query}"
+    get_all_tasks_headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    response = requests.get(get_all_tasks_url, headers=get_all_tasks_headers, verify=False, timeout=3600)
+    all_tasks = response.json()['content']
+    print(f"Задачи получены")
+    return all_tasks
+
+
+def generate_tasks_label(query):
+    sfera_query = f"area%20%3D%20%27{sfera_area}%27%20and%20label%20%3D%20%27{query}%27"
+    sfera_token = get_sfera_token()
+    tasks_pages_count = get_pages_count(sfera_token, sfera_query)
+    tasks_list = []
+    for page in range(0, tasks_pages_count):
+        print(f'Обработка {page+1} страницы из {tasks_pages_count+1}')
+        page_tasks = get_all_tasks(sfera_token, page, sfera_query)
+        for task in page_tasks:
+            try:
+                task_assignee = task['assignee']['name']
+            except:
+                task_assignee = 'Без исполнителя'
+
+            try:
+                task_systems = []
+                for system in task['systems']:
+                    task_systems.append(system['name'])
+            except Exception as e:
+                task_systems = 'Без системы'
+
+            try:
+                task_dueDate = task['dueDate']
+            except:
+                task_dueDate = 'Без срока исполнения'
+            try:
+                task_estimation = float(task['estimation'])
+                task_estimation = task_estimation/3600
+            except:
+                task_estimation = 0
+            try:
+                task_description = task['description']
+            except:
+                task_description = ' '
+            try:
+                task_relations = task['relations']
+            except:
+                task_relations = 'Без связи'
+            try:
+                task_labels = []
+                for label in task['label']:
+                    task_labels.append(label['name'])
+            except Exception as e:
+                task_labels = []
+            new_task_dict = {'number': task['number'], 'name': task['name'], 'description': task_description, 'status': task['status']['name'], 'assignee': task_assignee, 'systems': task_systems, 'date': task_dueDate, 'estimation': task_estimation, 'relation': task_relations, 'label': task_labels}
+            tasks_list.append(new_task_dict)
+    with open('tasks_dict.pickle', 'wb') as f:
+       pickle.dump(tasks_list, f)
+    return tasks_list
+
+
+def generate_tasks_dates(start_date, end_date, label):
+    sfera_query = f"area%20%3D%20%27{sfera_area}%27%20and%20dueDate%20%3C%3D%20%22{end_date}%22%20and%20dueDate%20%3E%3D%20%22{start_date}%22"
+    sfera_token = get_sfera_token()
+    tasks_pages_count = get_pages_count(sfera_token, sfera_query)
+    tasks_list = []
+    for page in range(0, tasks_pages_count):
+        print(f'Обработка {page+1} страницы из {tasks_pages_count+1}')
+        page_tasks = get_all_tasks(sfera_token, page, sfera_query)
+        for task in page_tasks:
+            try:
+                task_assignee = task['assignee']['name']
+            except:
+                task_assignee = 'Без исполнителя'
+
+            try:
+                task_systems = []
+                for system in task['systems']:
+                    task_systems.append(system['name'])
+            except Exception as e:
+                task_systems = 'Без системы'
+            try:
+                task_dueDate = task['dueDate']
+            except:
+                task_dueDate = 'Без срока исполнения'
+            try:
+                task_estimation = float(task['estimation'])
+                task_estimation = task_estimation/3600
+            except:
+                task_estimation = 0
+            try:
+                task_description = task['description']
+            except:
+                task_description = ' '
+            try:
+                task_relations = task['parent']
+            except:
+                task_relations = 'Без связи'
+            print(task)
+            try:
+                task_labels = []
+                for label in task['label']:
+                    task_labels.append(label['name'])
+            except Exception as e:
+                task_labels = []
+            new_task_dict = {'number': task['number'], 'name': task['name'], 'description': task_description, 'status': task['status']['name'], 'assignee': task_assignee, 'systems': task_systems, 'date': task_dueDate, 'estimation': task_estimation, 'relation': task_relations, 'label': task_labels}
+            tasks_list.append(new_task_dict)
+    with open('tasks_dict.pickle', 'wb') as f:
+       pickle.dump(tasks_list, f)
+    return tasks_list
