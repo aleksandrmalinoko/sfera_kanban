db8b832feb66fae1a2ae4fbf48d3dcd40b2843c8
aleksandrmalinoko <63043497+aleksandrmalinoko@users.noreply.github.com>
2026-02-14 14:29:15 +0300

    feat: configurable areas/orders and parent task rendering

--- utils/sfera_api.py
+++ utils/sfera_api.py
@@ -1,8 +1,8 @@
 import pickle
 import requests
-from datetime import datetime, timedelta
 import urllib3
 from utils import sfera_secrets
+from utils.app_config import DEFAULT_AREA
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -10,11 +10,34 @@ BASE_URL = "https://tasks.example.local"
 TASK_URL = f"{BASE_URL}/app/tasks/api/v1/entities"
 LOGIN_URL = f"{BASE_URL}/api/auth/login"
 sfera_attributes = ['number', 'name', 'description', 'relations', 'status', 'assignee', 'systems', 'dueDate', 'estimation', 'label', 'parent']
-sfera_area = 'AREA1'
 atributes = ''
 for atr in sfera_attributes[:-1]:
     atributes += f'{atr}%2C'
 atributes += sfera_attributes[-1]
+
+
+def normalize_parent(parent_raw):
+    if not parent_raw:
+        return []
+
+    parent_items = parent_raw if isinstance(parent_raw, list) else [parent_raw]
+    normalized = []
+
+    for item in parent_items:
+        if not isinstance(item, dict):
+            continue
+        parent_number = item.get('number')
+        parent_name = item.get('name')
+        parent_area = item.get('area') or DEFAULT_AREA
+
+        if parent_number:
+            normalized.append({
+                'number': parent_number,
+                'name': parent_name or parent_number,
+                'area': parent_area,
+            })
+
+    return normalized
 
 
 def get_sfera_token():
@@ -55,8 +78,8 @@ def get_all_tasks(token, page, sfera_query):
     return all_tasks
 
 
-def generate_tasks_label(query):
-    sfera_query = f"area%20%3D%20%27{sfera_area}%27%20and%20label%20%3D%20%27{query}%27"
+def generate_tasks_label(query, area):
+    sfera_query = f"area%20%3D%20%27{area}%27%20and%20label%20%3D%20%27{query}%27"
     sfera_token = get_sfera_token()
     tasks_pages_count = get_pages_count(sfera_token, sfera_query)
     tasks_list = []
@@ -89,25 +112,34 @@ def generate_tasks_label(query):
                 task_description = task['description']
             except:
                 task_description = ' '
-            try:
-                task_relations = task['relations']
-            except:
-                task_relations = 'Без связи'
+            task_parents = normalize_parent(task.get('parent'))
             try:
                 task_labels = []
                 for label in task['label']:
                     task_labels.append(label['name'])
             except Exception as e:
                 task_labels = []
-            new_task_dict = {'number': task['number'], 'name': task['name'], 'description': task_description, 'status': task['status']['name'], 'assignee': task_assignee, 'systems': task_systems, 'date': task_dueDate, 'estimation': task_estimation, 'relation': task_relations, 'label': task_labels}
+            new_task_dict = {
+                'number': task['number'],
+                'name': task['name'],
+                'description': task_description,
+                'status': task['status']['name'],
+                'assignee': task_assignee,
+                'systems': task_systems,
+                'date': task_dueDate,
+                'estimation': task_estimation,
+                'parents': task_parents,
+                'label': task_labels,
+                'area': area,
+            }
             tasks_list.append(new_task_dict)
     with open('tasks_dict.pickle', 'wb') as f:
        pickle.dump(tasks_list, f)
     return tasks_list
 
 
-def generate_tasks_dates(start_date, end_date, label):
-    sfera_query = f"area%20%3D%20%27{sfera_area}%27%20and%20dueDate%20%3C%3D%20%22{end_date}%22%20and%20dueDate%20%3E%3D%20%22{start_date}%22"
+def generate_tasks_dates(start_date, end_date, label, area):
+    sfera_query = f"area%20%3D%20%27{area}%27%20and%20dueDate%20%3C%3D%20%22{end_date}%22%20and%20dueDate%20%3E%3D%20%22{start_date}%22"
     sfera_token = get_sfera_token()
     tasks_pages_count = get_pages_count(sfera_token, sfera_query)
     tasks_list = []
@@ -139,18 +171,26 @@ def generate_tasks_dates(start_date, end_date, label):
                 task_description = task['description']
             except:
                 task_description = ' '
-            try:
-                task_relations = task['parent']
-            except:
-                task_relations = 'Без связи'
-            print(task)
+            task_parents = normalize_parent(task.get('parent'))
             try:
                 task_labels = []
                 for label in task['label']:
                     task_labels.append(label['name'])
             except Exception as e:
                 task_labels = []
-            new_task_dict = {'number': task['number'], 'name': task['name'], 'description': task_description, 'status': task['status']['name'], 'assignee': task_assignee, 'systems': task_systems, 'date': task_dueDate, 'estimation': task_estimation, 'relation': task_relations, 'label': task_labels}
+            new_task_dict = {
+                'number': task['number'],
+                'name': task['name'],
+                'description': task_description,
+                'status': task['status']['name'],
+                'assignee': task_assignee,
+                'systems': task_systems,
+                'date': task_dueDate,
+                'estimation': task_estimation,
+                'parents': task_parents,
+                'label': task_labels,
+                'area': area,
+            }
             tasks_list.append(new_task_dict)
     with open('tasks_dict.pickle', 'wb') as f:
        pickle.dump(tasks_list, f)
